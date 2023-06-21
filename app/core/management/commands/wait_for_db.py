593d2ebdcea2bcc450f245cbcfcb9023ed9d0fb5
Ryan Taylor <rctaylor123@gmail.com>
2023-06-21 10:42:04 -0600

    fix some linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ Django command to wait for the database to be available
 """
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for database"""
 
